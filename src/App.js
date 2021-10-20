dfade099dcaccbcd91b8123c321e3d91666f63e8
Swaraj Patil <swarajspatil158@gmail.com>
2021-10-20 10:02:29 +0530

    updateList removeList draggable added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./Home"
 import "./index.scss";
 
+
+
 export default function App() {
  
   return (
--- src/Home/index.js
+++ src/Home/index.js
@@ -48,6 +48,36 @@ export default function Home() {
     setData(newState);
     window.localStorage.setItem("dataKanban", JSON.stringify(newState));
   };
+  const removeCard = (index, listId) => {
+    const list = data.lists[listId];
+
+    list.cards.splice(index, 1);
+
+    const newState = {
+      ...data,
+      lists: {
+        ...data.lists,
+        [listId]: list,
+      },
+    };
+    setData(newState);
+    window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+  };
+
+  const updateCardTitle = (title, index, listId) => {
+    const list = data.lists[listId];
+    list.cards[index].title = title;
+
+    const newState = {
+      ...data,
+      lists: {
+        ...data.lists,
+        [listId]: list,
+      },
+    };
+    setData(newState);
+    window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+  };
   const addMoreList = (title) => {
     if (!title) {
       return;
@@ -69,14 +99,111 @@ export default function Home() {
     setData(newState);
     window.localStorage.setItem("dataKanban", JSON.stringify(newState));
   };
+
+  const updateListTitle = (title, listId) => {
+    const list = data.lists[listId];
+    list.title = title;
+
+    const newState = {
+      ...data,
+      lists: {
+        ...data.lists,
+        [listId]: list,
+      },
+    };
+
+    setData(newState);
+    window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+  };
+
+  const deleteList = (listId) => {
+    const lists = data.lists;
+    const listIds = data.listIds;
+
+    delete lists[listId];
+
+    listIds.splice(listIds.indexOf(listId), 1);
+
+    const newState = {
+      lists: lists,
+      listIds: listIds,
+    };
+
+    setData(newState);
+    window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+  };
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (type === "list") {
+      const newListIds = data.listIds;
+
+      newListIds.splice(source.index, 1);
+      newListIds.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...data,
+        listIds: newListIds,
+      };
+      setData(newState);
+      window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+
+      return;
+    }
+
+    const sourceList = data.lists[source.droppableId];
+    const destinationList = data.lists[destination.droppableId];
+    const draggingCard = sourceList.cards.filter(
+      (card) => card.id === draggableId
+    )[0];
+
+    if (source.droppableId === destination.droppableId) {
+      sourceList.cards.splice(source.index, 1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
+
+      const newState = {
+        ...data,
+        lists: {
+          ...data.lists,
+          [sourceList.id]: destinationList,
+        },
+      };
+      setData(newState);
+      window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+    } else {
+      sourceList.cards.splice(source.index, 1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
+
+      const newState = {
+        ...data,
+        lists: {
+          ...data.lists,
+          [sourceList.id]: sourceList,
+          [destinationList.id]: destinationList,
+        },
+      };
+
+      setData(newState);
+      window.localStorage.setItem("dataKanban", JSON.stringify(newState));
+    }
+  };
   return (
     <StoreApi.Provider
       value={{
         addMoreCard,
-        addMoreList
+        addMoreList,
+        updateListTitle,
+        removeCard,
+        updateCardTitle,
+        deleteList
       }}
     >
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="app" type="list" direction="horizontal">
           {(provided) => (
             <div
